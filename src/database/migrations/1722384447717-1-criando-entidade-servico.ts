db88d301b35cb5bd0d4cd93f595d45f056386662
matheusrodrigues1 <matech00@outlook.com>
2024-07-30 21:13:54 -0300

    criando primeira migration

---
+++ src/database/migrations/1722384447717-1-criando-entidade-servico.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CriandoEntidadeServico1722384447717 implements MigrationInterface {
+  name = '1CriandoEntidadeServico1722384447717';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE \`servico\` (\`id\` int NOT NULL AUTO_INCREMENT, \`nome\` varchar(255) NOT NULL, \`valor_minimo\` int NOT NULL, \`quantidadede_horas\` int NOT NULL, \`porcentagem\` int NOT NULL, \`valor_quarto\` int NOT NULL, \`horas_quarto\` int NOT NULL, \`valor_sala\` int NOT NULL, \`horas_sala\` int NOT NULL, \`valor_banheiro\` int NOT NULL, \`horas_banheiro\` int NOT NULL, \`valor_conzinha\` int NOT NULL, \`horas_cozinha\` int NOT NULL, \`valor_quintal\` int NOT NULL, \`horas_quintal\` int NOT NULL, \`valor_outros\` int NOT NULL, \`horas_outros\` int NOT NULL, \`icone\` varchar(255) NOT NULL, \`posicao\` int NOT NULL, \`created_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`update_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE \`servico\``);
+  }
+}
