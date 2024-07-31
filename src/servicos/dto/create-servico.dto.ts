e87d6e5cfef32be5f03b80184ecccc4a630342d1
matheusrodrigues1 <matech00@outlook.com>
2024-07-31 12:30:59 -0300

    cadastrando servico

--- src/servicos/dto/create-servico.dto.ts
+++ src/servicos/dto/create-servico.dto.ts
@@ -1,1 +1,44 @@
-export class CreateServicoDto {}
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export class CreateServicoDto {
+  nome: string;
+
+  valorMinimo: number;
+
+  quantidadedeHoras: number;
+
+  porcentagem: number;
+
+  valorQuarto: number;
+
+  horasQuarto: number;
+
+  valorSala: number;
+
+  horasSala: number;
+
+  valorBanheiro: number;
+
+  horasBanheiro: number;
+
+  valorConzinha: number;
+
+  horasCozinha: number;
+
+  valorQuintal: number;
+
+  horasQuintal: number;
+
+  valorOutros: number;
+
+  horasOutros: number;
+
+  icone: string;
+
+  posicao: number;
+}
--- src/servicos/servicos.module.ts
+++ src/servicos/servicos.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ServicosService } from './servicos.service';
 import { ServicosController } from './servicos.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Servico } from './entities/servico.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Servico])],
   controllers: [ServicosController],
-  providers: [ServicosService]
+  providers: [ServicosService],
 })
 export class ServicosModule {}
