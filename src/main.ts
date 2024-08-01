7d36686c1108cdeb0523bb2f4598737f97115df5
matheusrodrigues1 <matech00@outlook.com>
2024-08-01 13:11:19 -0300

    validação com class-validator class-transformer

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as methodOverride from 'method-override';
 import * as exphbs from 'express-handlebars';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const viewPatch = join(__dirname, '..', 'views');
+  app.useGlobalPipes(new ValidationPipe());
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(viewPatch);
   app.setViewEngine('hbs');
   app.engine('hbs', exphbs.engine({ extname: 'hbs', defaultLayout: 'main' }));
+
+  app.use(methodOverride('_method'));
 
   await app.listen(3000);
 }
--- src/servicos/dto/create-servico.dto.ts
+++ src/servicos/dto/create-servico.dto.ts
@@ -1,37 +1,135 @@
+import { Type } from 'class-transformer';
+import {
+  Length,
+  IsNotEmpty,
+  IsCurrency,
+  Matches,
+  IsNumber,
+  Max,
+  Min,
+} from 'class-validator';
+
 export class CreateServicoDto {
+  @IsNotEmpty({ message: 'Campo nome não pode ser vazio' })
+  @Length(3, 99, { message: 'Campo nome deve ter entre 3 e 99 caracteres' })
   nome: string;
 
+  @IsCurrency(
+    { thousands_separator: '.', decimal_separator: ',' },
+    { message: 'Campo valor mínimo com valor invalido' },
+  )
+  @Matches(RegExp('^((?![-]).)*$'), {
+    message: 'Valor mínimo não pode ser negativo',
+  })
   valorMinimo: number;
 
+  @IsNumber()
+  @Min(0)
+  @Max(10)
+  @Type(() => Number)
   quantidadedeHoras: number;
 
+  @IsNumber()
+  @Min(0)
+  @Max(100)
+  @Type(() => Number)
   porcentagem: number;
 
+  @IsCurrency(
+    { thousands_separator: '.', decimal_separator: ',' },
+    { message: 'Campo valor mínimo com valor invalido' },
+  )
+  @Matches(RegExp('^((?![-]).)*$'), {
+    message: 'Valor mínimo não pode ser negativo',
+  })
   valorQuarto: number;
 
+  @IsNumber()
+  @Min(0)
+  @Max(10)
+  @Type(() => Number)
   horasQuarto: number;
 
+  @IsCurrency(
+    { thousands_separator: '.', decimal_separator: ',' },
+    { message: 'Campo valor mínimo com valor invalido' },
+  )
+  @Matches(RegExp('^((?![-]).)*$'), {
+    message: 'Valor mínimo não pode ser negativo',
+  })
   valorSala: number;
 
+  @IsNumber()
+  @Min(0)
+  @Max(10)
+  @Type(() => Number)
   horasSala: number;
 
+  @IsCurrency(
+    { thousands_separator: '.', decimal_separator: ',' },
+    { message: 'Campo valor mínimo com valor invalido' },
+  )
+  @Matches(RegExp('^((?![-]).)*$'), {
+    message: 'Valor mínimo não pode ser negativo',
+  })
   valorBanheiro: number;
 
+  @IsNumber()
+  @Min(0)
+  @Max(10)
+  @Type(() => Number)
   horasBanheiro: number;
 
+  @IsCurrency(
+    { thousands_separator: '.', decimal_separator: ',' },
+    { message: 'Campo valor mínimo com valor invalido' },
+  )
+  @Matches(RegExp('^((?![-]).)*$'), {
+    message: 'Valor mínimo não pode ser negativo',
+  })
   valorConzinha: number;
 
+  @IsNumber()
+  @Min(0)
+  @Max(10)
+  @Type(() => Number)
   horasCozinha: number;
 
+  @IsCurrency(
+    { thousands_separator: '.', decimal_separator: ',' },
+    { message: 'Campo valor mínimo com valor invalido' },
+  )
+  @Matches(RegExp('^((?![-]).)*$'), {
+    message: 'Valor mínimo não pode ser negativo',
+  })
   valorQuintal: number;
 
+  @IsNumber()
+  @Min(0)
+  @Max(10)
+  @Type(() => Number)
   horasQuintal: number;
 
+  @IsCurrency(
+    { thousands_separator: '.', decimal_separator: ',' },
+    { message: 'Campo valor mínimo com valor invalido' },
+  )
+  @Matches(RegExp('^((?![-]).)*$'), {
+    message: 'Valor mínimo não pode ser negativo',
+  })
   valorOutros: number;
 
+  @IsNumber()
+  @Min(0)
+  @Max(10)
+  @Type(() => Number)
   horasOutros: number;
 
+  @IsNotEmpty({ message: 'Icone nao pode ser vazio' })
   icone: string;
 
+  @IsNumber({}, { message: 'Icone nao pode ser vazio' })
+  @Min(1)
+  @Type(() => Number)
   posicao: number;
 }
