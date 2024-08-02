f738cfd6c126d60149f7bc786a4598eec7d36742
matheusrodrigues1 <matech00@outlook.com>
2024-08-02 20:55:04 -0300

    config modulo usuario plataforma

--- src/usuario-plataforma/usuario-plataforma.module.ts
+++ src/usuario-plataforma/usuario-plataforma.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsuarioPlataformaService } from './usuario-plataforma.service';
 import { UsuarioPlataformaController } from './usuario-plataforma.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsuarioPlataforma } from './entities/usuario-plataforma.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UsuarioPlataforma])],
   controllers: [UsuarioPlataformaController],
-  providers: [UsuarioPlataformaService]
+  providers: [UsuarioPlataformaService],
 })
 export class UsuarioPlataformaModule {}
