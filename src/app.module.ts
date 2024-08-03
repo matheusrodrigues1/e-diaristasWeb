7a55f3412898839389e4977cf4bba88e649ce2de
matheusrodrigues1 <matech00@outlook.com>
2024-08-03 17:47:55 -0300

    criando view de login e instalando passport e estrutura de login

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './database/typeorm-config';
 import { ServicosModule } from './servicos/servicos.module';
 import { UsuarioPlataformaModule } from './usuario-plataforma/usuario-plataforma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { UsuarioPlataformaModule } from './usuario-plataforma/usuario-plataforma
     }),
     ServicosModule,
     UsuarioPlataformaModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
