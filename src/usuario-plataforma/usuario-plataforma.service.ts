1e0faa27055af64b27739e7e904e3df48142bcd9
matheusrodrigues1 <matech00@outlook.com>
2024-08-02 21:18:30 -0300

    config service cadastrar usuario

--- src/usuario-plataforma/usuario-plataforma.service.ts
+++ src/usuario-plataforma/usuario-plataforma.service.ts
@@ -1,11 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUsuarioPlataformaDto } from './dto/create-usuario-plataforma.dto';
 import { UpdateUsuarioPlataformaDto } from './dto/update-usuario-plataforma.dto';
+import { BeforeInsert, Repository } from 'typeorm';
+import { UsuarioPlataforma } from './entities/usuario-plataforma.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsuarioPlataformaService {
-  create(createUsuarioPlataformaDto: CreateUsuarioPlataformaDto) {
-    return 'This action adds a new usuarioPlataforma';
+  constructor(
+    @InjectRepository(UsuarioPlataforma)
+    private usuarioRepository: Repository<UsuarioPlataforma>,
+  ) {}
+
+  async create(createUsuarioPlataformaDto: CreateUsuarioPlataformaDto) {
+    const user = await this.usuarioRepository.findOneBy({
+      email: createUsuarioPlataformaDto.email,
+    });
+
+    if (
+      createUsuarioPlataformaDto.password !=
+      createUsuarioPlataformaDto.passwordConfirmation
+    ) {
+      throw new BadRequestException('Senha nao conferem');
+    } else if (!user) {
+      createUsuarioPlataformaDto.password = await this.setPassword(
+        createUsuarioPlataformaDto.password,
+      );
+
+      return this.usuarioRepository.save(createUsuarioPlataformaDto);
+    }
+
+    throw new BadRequestException('Email ja cadastrado');
   }
 
   findAll() {
@@ -22,5 +48,12 @@ export class UsuarioPlataformaService {
 
   remove(id: number) {
     return `This action removes a #${id} usuarioPlataforma`;
+  }
+
+  @BeforeInsert()
+  async setPassword(password: string) {
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+    return passwordHash;
   }
 }
